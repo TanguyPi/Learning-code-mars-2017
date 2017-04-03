80dd02ad86e554cfa6f3153d1ab143b866903183
Etudiant <Etudiant>
2017-04-03 17:11:36 +0200

    31mars

--- 3 avril/js/script.js
+++ 3 avril/js/script.js
@@ -1,6 +1,9 @@
 // Chargement du DOM
 $(document).ready(function(){
-    console.log('Dom chargé')
+    
+
+    // Créer un tableau pour enregistrer les données des utilisateurs en bas du site ''statistiques de votre ville''
+    var myTown = {}
 
 
 
@@ -11,13 +14,10 @@ $(document).ready(function(){
         var avatarGender; 
 
        // Avatar capter le click quand qqun click sur femme ou sur homme (on le sort du formulaire(form) car on ne veut pas appuyer sur le submit pour stipuler son genre)
-        avatarWoman.click(function(){
-                console.log('Je suis une' + avatarWoman.val());
-              });   
+        avatarWoman.click(function(){    
 
         avatarMan.click(function(){
-            console.log('Je suis un' + avatarMan.val());
-   
+          
 
         //   désactiver avatarWoman quand on clique sur homme et vise versa
         avatarWoman.prop('checked',false);
@@ -34,15 +34,35 @@ $(document).ready(function(){
         
         // Vider le message d'erreur pour les femmes ( cas spécifiques ici pas de label)
         $('.labelGender b').text('');
-        });
+    
 
-
-
+        // Modifier l'attribut src de #avatarBody (photo de fille et de mec qui change en fonction du choix de l'utilisateur. )
+        $('#avatarBody').attr('src' + 'img/' + avatarGender + '.png')
+         });
 
         // Supprimer les messages d'erreurs quand on clique sur l'endroit ou on va écrire
         $('input,select').focus(function(){
                 // Sélectionner la balise précédente le input (les labels donc et on va utliser les balsies b qu'on a mise)
                 $(this).prev().children('b').text('');
+
+    
+                 });
+
+        // Capter l'événement change() sur les selects
+        $('select').change(function(){
+            console.log(($this).attr('id'), 'change :' + $(this).val());
+
+            // Condition if pour modifier la valeur src de avatarBottom
+            if($(this).attr('id') == 'avatarStyleTop'){
+                console.log('modification du style top');
+
+                // Modifier la valeur de l'attribut src de #avatarTop
+                $('#avatarTop').attr('src','img/top' + $(this).val() +'.png');
+
+            }else{
+                $('#avatarBottom').attr('src','img/bottom' + $(this).val() +'.png');
+
+            };
 
         });
 
@@ -132,12 +152,81 @@ $(document).ready(function(){
             console.log(formscore);{ 
             console.log('Le formulaire est validé !');
         };
-                // Envoyer les données du formulaire et attendre la réponse du serveur
+                // Envoyer les données du formulaire et attendre la réponse du serveur en Ajax
                 // Le serveur répond true
 
+                // Ajouter une ligne dans le tableau html en bas du site
+                $('table').append('' +
+                    '<tr>'+
+                    '<td><b>' + avatarName + '</b></td>'+
+                    '<td>' + avatarAge + 'an(s)</td>'+
+                    '<td>' + avatarGender +'</td>'+
+                    '<td>' + avatarStyleTop +'</td>'+
+                '</tr>'
+                );
+
+
+                // Ajouter l'avatar du formulaire dans le tableau(c'est un tableau objet) JS
+                myTown.push({
+                    name: avatarName,
+                    gender:avatarGender,
+                    age:parseInt(avatarAge),
+                    top:avatarStyleTop,
+                    bottom:avatarStyleBottom,
+                    
+                });
+                
+               
+               
                 // Vider les champs du formulaire au moment ou l'utilisateur clique sur valider afin que l'utilisateur sache que son formulaire est validé
                 $('form')[0].reset();
 
+                // Revenir aux valeurs 'null' pour l'avatar cad le mecn'aura pas dhabit sur lui
+                $('#avatarBody').attr('src', 'img/null.png');
+                $('#avatarBottom').attr('src', 'img/bottom/null.png');
+                $('#avatarTop').attr('src', 'img/top/null.png');
+
+
+
+                // Afficher les données du tableau dans la console
+                console.log(myTown.length);
+                        // Afficher la taille totale de la ville dans #totalSims
+                        $('#totalSims');
+                        $('#simsWoman b, simsMan b').text('/' + myTown.length);
+
+
+                        // définir les variables globales pour le statistiques
+                        var totalGirls= 0;
+                        var totalBoys=0;
+                        var totalAge=0;
+
+                        // Faire une boucle for sur myTown pour connaitre les statistiques
+                        for(var i=0;i<myTown.length; i++){
+                            console.log(myTown[i].gender);
+
+                            // Condition pour le gender (totalgirls++ car ++= 1)
+                            if(myTown[i].gender=='girl'){
+                                totalGirls++;
+
+                            }else{
+                                totalBoys++;
+                            }
+  
+                            // Additionner les ages
+                            totalAge += totalAge + myTown[i].age
+
+                        };
+                        // Afficher dans le tableau html le nombre de girls et le nombre de boys
+                        $('#simsWoman').html(totalGirls) + '<b>/' + myTown.length +'</b');
+                        $('#simsMan').html(totalBoys) + '<b>/' + myTown.length +'</b');
+
+                        // Afficher l'age total dans la console
+                       var ageAmountRounded = Math.round(totalAge/myTown.length);
+                       $('simsAgeAmount').html(ageAmountRounded + '<b>/ans</b>');
+
+                          };
+               
+  });
 
 
      
